78faac325b3d9543e2d4af05ee44a9a90327f556
kadershaikh <kadershaikh666@gmail.com>
2022-01-13 17:14:55 +0530

    correction in dashboard controller for chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -11,12 +12,23 @@ class DashboardController extends Controller
     {
         $year = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+        $monthly_user = DB::table('users')
+            ->select(DB::raw('count(id) as total'), DB::raw('MONTH(created_at) as month'))
+            ->groupBy('month')
+            ->get();
+
+        foreach ($monthly_user as $key) {
+            $year[$key->month - 1] = $key->total;
+        }
+
         $year1 = ['2022', '2023'];
 
         $user = [];
         foreach ($year1 as $key => $value) {
             $user[] = User::where(\DB::raw("DATE_FORMAT(created_at, '%Y')"), $value)->count();
         }
+
+        // dd($user);
 
         return view('dashboard')->with('year', $year)->with('user', $user)->with('year1', $year1);
     }
